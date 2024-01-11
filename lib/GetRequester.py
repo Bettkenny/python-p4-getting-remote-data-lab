d4b274dc4901ba55a6eadb141c56b09f50deac65
YOUR NAME <YOUR@EMAIL.com>
2024-01-11 11:30:33 +0300

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,22 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
+        response = requests.get(self.url)
+        
+        if response.status_code == 200:
+            return response.content  
+            
+            raise Exception(f"Failed to fetch data. Status code: {response.status_code}")
+
         pass
 
     def load_json(self):
+        response_body = self.get_response_body()
+   
+        try:
+            json_data = json.loads(response_body.decode('utf-8'))  
+            return json_data
+        except json.JSONDecodeError as e:
+
+            raise Exception(f"Failed to load JSON: {str(e)}")
         pass
